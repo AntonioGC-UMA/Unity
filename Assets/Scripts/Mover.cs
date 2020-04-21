020c5cea5adfa5a1fac2ffaf8a110ff3102eee54
AntonioGC <AntonioGC@uma.es>
2020-04-21 17:08:27 +0200

    Primer comit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float velocida;
+
+    public Rigidbody2D rb;
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(velocida, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-velocida, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.velocity = new Vector2(0, velocida);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.velocity = new Vector2(0, -velocida);
+        }
+    }
+}
